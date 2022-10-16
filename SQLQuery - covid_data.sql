47f3a5bd7b4e5ec0d0f8dcc506102297a74c5722
kwong577 <113663214+kwong577@users.noreply.github.com>
2022-10-16 01:00:31 -0400

    Add files via upload

---
+++ SQLQuery - covid_data.sql
@@ -0,0 +1,231 @@
+-- Browse the entire dataset for covid_deaths
+SELECT *
+FROM PortfolioProject..covid_deaths
+ORDER BY 3,4
+
+--- Browse the entire dataset for covid_vaccinations
+SELECT *
+FROM PortfolioProject..covid_vaccinations
+ORDER BY 3,4
+
+--- Browse the selected columns from the dataset for covid_deaths
+SELECT location, date, total_cases, new_cases, total_deaths, population
+FROM PortfolioProject..covid_deaths
+ORDER BY 1,2
+
+--- Explore total_cases vs total_deaths for Canada
+-- Likelihood of dying from contracting covid (not accounting vaccinations)
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+FROM PortfolioProject..covid_deaths
+WHERE location like 'Canada'
+ORDER BY 1,2
+
+--- Create a view to store data for visualization
+DROP VIEW if exists death_percentage_canada
+CREATE VIEW death_percentage_canada as
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
+FROM PortfolioProject..covid_deaths
+WHERE location like 'Canada'
+
+--- Explore total_cases vs population for Canada
+-- Percentage of the population that contracted covid overtime
+Select location, date, total_cases, population, (total_cases/population)*100 as cases_per_population_percentage
+From PortfolioProject..covid_deaths
+WHERE location like 'Canada'
+Order By 2
+
+--- Create a view to store data for visualization
+DROP VIEW if exists cases_per_population_percentage_canada
+CREATE VIEW cases_per_population_percentage_canada as
+Select location, date, total_cases, population, (total_cases/population)*100 as cases_per_population_percentage
+From PortfolioProject..covid_deaths
+WHERE location like 'Canada'
+
+--- Explore max infection count vs their population
+--SELECT location, population, MAX(total_cases) as max_infection_count, MAX((total_cases/population))*100 as infected_population_percentage
+--FROM PortfolioProject..covid_deaths
+--GROUP BY location, population
+--ORDER BY infected_population_percentage desc
+--- We realize that there are rows containing grouped data (world, high income, asia, etc.) and not just for an individual country 
+--- Looking at the continent and location columns, null values for continent indicate that that row contains grouped data so we need to exclude it using WHERE
+
+--- Explore max infection count vs their population
+-- Percentage of the population that ended up contracting covid
+SELECT location, population, MAX(total_cases) as max_infection_count, MAX((total_cases/population))*100 as infected_population_percentage
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null
+GROUP BY location, population
+ORDER BY infected_population_percentage DESC
+
+--- Create a view to store data for visualization
+DROP VIEW if exists infected_population_percentage
+CREATE VIEW infected_population_percentage as
+SELECT location, population, MAX(total_cases) as max_infection_count, MAX((total_cases/population))*100 as infected_population_percentage
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null
+GROUP BY location, population
+
+--- Explore countries with the highest death count
+--Select location, MAX(total_deaths) as max_death_count
+--From PortfolioProject..covid_deaths
+--Group By location 
+--Order By max_death_count desc
+--- We realize the data type of total_deaths is nvarchar so we need to change it into integer using CAST
+
+--- Explore countries with the highest death count 
+-- Total death count by country
+SELECT location, MAX(CAST(total_deaths as int)) as max_death_count
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null
+GROUP BY location 
+ORDER BY max_death_count DESC
+
+--- Create a view to store data for visualization
+DROP VIEW if exists max_death_count_countries
+CREATE VIEW max_death_count_countries as
+SELECT location, MAX(CAST(total_deaths as int)) as max_death_count
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null
+GROUP BY location 
+
+--- Explore the highest death count within the grouped data we previous found under the locations column
+-- Total death count by predefined groups within the dataset
+SELECT location, MAX(CAST(total_deaths as int)) as max_death_count
+FROM PortfolioProject..covid_deaths
+WHERE continent is null
+GROUP BY location 
+ORDER BY max_death_count DESC
+
+--- Explore the highest death count within the grouped data involving continents we previous found under the locations column
+-- Total death count by predefined continents within the dataset
+SELECT location, MAX(CAST(total_deaths as int)) as max_death_count
+FROM PortfolioProject..covid_deaths
+WHERE continent is null AND location not in ('World', 'High income', 'Upper middle income', 'lower middle income', 'low income')
+GROUP BY location 
+ORDER BY max_death_count DESC
+
+--- Create a view to store data for visualization
+DROP VIEW if exists max_death_count_continent
+CREATE VIEW max_death_count_continent as
+SELECT location, MAX(CAST(total_deaths as int)) as max_death_count
+FROM PortfolioProject..covid_deaths
+WHERE continent is null AND location not in ('World', 'High income', 'Upper middle income', 'lower middle income', 'low income')
+GROUP BY location 
+
+--- Explore the number of new_cases vs new_deaths from a global perspective per day
+-- Sum of new cases and deaths that occurred in the world each day
+-- Percentage of new deaths from new cases that occurred each day
+SELECT date, SUM(new_cases) as total_new_cases, SUM(CAST(new_deaths as int)) as total_new_deaths, (SUM(CAST(new_deaths as int))/SUM(new_cases))*100 as new_case_death_percentage
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null
+GROUP BY date 
+ORDER BY 1
+
+--- Create a view to store data for visualization
+DROP VIEW if exists new_case_death_percentage
+CREATE VIEW new_case_death_percentage as
+SELECT date, SUM(new_cases) as total_new_cases, SUM(CAST(new_deaths as int)) as total_new_deaths, (SUM(CAST(new_deaths as int))/SUM(new_cases))*100 as new_case_death_percentage
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null
+GROUP BY date 
+
+--- Explore the total number of new_cases vs new_deaths from a global perspective 
+-- Total number of new cases and deaths that occurred in the world (as of most recent date in the dataset)
+-- Percentage of deaths from cases that occurred (as of most recent date in the dataset)
+SELECT SUM(new_cases) as total_new_cases, SUM(CAST(new_deaths as int)) as total_new_deaths, (SUM(CAST(new_deaths as int))/SUM(new_cases))*100 as new_case_death_percentage
+FROM PortfolioProject..covid_deaths
+WHERE continent is not null 
+
+--- Join the covid_deaths and covid_vaccination datasets together by location and date
+SELECT *
+FROM PortfolioProject..covid_deaths dea
+JOIN PortfolioProject..covid_vaccinations vac
+ON dea.location = vac.location AND dea.date = vac.date
+
+--- Browse the selected columns from the joined dataset 
+-- Recall that there is predefined grouped date in the locations column which we need to exclude to only pull date for each individual country
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+FROM PortfolioProject..covid_deaths dea
+JOIN PortfolioProject..covid_vaccinations vac
+ON dea.location = vac.location AND dea.date = vac.date
+WHERE dea.continent is not null
+ORDER BY 2, 3
+
+--- Create a rolling count of new vaccinations for each country
+--SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rolling_vaccination_count
+--FROM PortfolioProject..covid_deaths dea
+--JOIN PortfolioProject..covid_vaccinations vac
+--on dea.location = vac.location AND dea.date = vac.date
+--WHERE dea.continent is not null
+--ORDER BY 2, 3
+--- We realize the data type of total_vaccinations is nvarchar so we need to change it into integer using CAST
+--- An error occurred when we tried to convert to int due to exceeding range so we used bigint 
+
+--- Create a rolling count of new vaccinations for each country
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CAST(vac.new_vaccinations as bigint)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rolling_vaccination_count
+FROM PortfolioProject..covid_deaths dea
+JOIN PortfolioProject..covid_vaccinations vac
+on dea.location = vac.location AND dea.date = vac.date
+WHERE dea.continent is not null
+ORDER BY 2, 3
+
+--- Create a new table for the above so that we can perform new calculations with our rolling_vaccination_count column
+DROP TABLE if exists population_vaccination
+CREATE TABLE population_vaccination
+(
+continent nvarchar(255),
+location nvarchar(225),
+date datetime,
+population numeric,
+new_vaccinations numeric,
+rolling_vaccination_count numeric
+)
+INSERT INTO population_vaccination
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CAST(vac.new_vaccinations as bigint)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as rolling_vaccination_count
+FROM PortfolioProject..covid_deaths dea
+JOIN PortfolioProject..covid_vaccinations vac
+on dea.location = vac.location AND dea.date = vac.date
+WHERE dea.continent is not null
+
+--- Explore the rolling_vaccination count vs population
+-- Percentage of the population vaccinated based on rolling count of vaccinations for each country
+SELECT *, (rolling_vaccination_count/population)*100 as vaccinated_population_percentage
+FROM population_vaccination
+ORDER BY 2, 3
+
+--- Create a view to store data for visualization
+DROP VIEW if exists dbo.vaccinated_population_percentage
+CREATE VIEW vaccinated_population_percentage as
+SELECT *, (rolling_vaccination_count/population)*100 as vaccinated_population_percentage
+FROM population_vaccination
+
+--- Explore countries with the highest vaccination count 
+-- Total vaccination count by country
+SELECT location, MAX(rolling_vaccination_count) as total_vaccination_count
+FROM population_vaccination
+GROUP BY location 
+ORDER BY total_vaccination_count DESC
+
+--- Create a view to store data for visualization
+DROP VIEW if exists total_vaccination_count
+CREATE VIEW total_vaccination_count as
+SELECT location, MAX(rolling_vaccination_count) as total_vaccination_count
+FROM population_vaccination
+GROUP BY location 
+
+--- Explore the highest vaccination count within the grouped data involving continents we previous found under the locations column
+-- Total vaccination count by predefined continents within the dataset
+SELECT location, MAX(CAST(total_vaccinations as bigint)) as total_vaccination_count
+FROM PortfolioProject..covid_vaccinations
+WHERE continent is null AND location not in ('World', 'High income', 'Upper middle income', 'lower middle income', 'low income')
+GROUP BY location 
+ORDER BY total_vaccination_count DESC
+
+--- Create a view to store data for visualization
+DROP VIEW if exists total_vaccination_count_continent
+CREATE VIEW total_vaccination_count_continent as
+SELECT location, MAX(CAST(total_vaccinations as bigint)) as total_vaccination_count
+FROM PortfolioProject..covid_vaccinations
+WHERE continent is null AND location not in ('World', 'High income', 'Upper middle income', 'lower middle income', 'low income')
+GROUP BY location
+
